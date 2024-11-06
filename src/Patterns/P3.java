ba96d807ffd86e16d99369abf2f757b085e441c9
Sooraj Hoysal <surajhoysal817@gmail.com>
2024-11-06 09:40:20 +0530

    basic patterns

---
+++ src/Patterns/P3.java
@@ -0,0 +1,20 @@
+package Patterns;
+
+public class P3 {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i + 1; j++){
+                System.out.print(j + 1 + " ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
+
+
+//       1
+//        1 2
+//        1 2 3
+//        1 2 3 4
+//        1 2 3 4 5
